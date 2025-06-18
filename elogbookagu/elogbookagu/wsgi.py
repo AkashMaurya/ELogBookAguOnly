f3423c993a855f76c71c2de8fdc21e4c97107824
Akash <akashmaur94@gmail.com>
2025-06-18 07:00:29 +0300

    Fix: corrected DJANGO_SETTINGS_MODULE path in render.yaml

--- elogbookagu/elogbookagu/wsgi.py
+++ elogbookagu/elogbookagu/wsgi.py
@@ -17,8 +17,9 @@ application = get_wsgi_application()"""
 
 
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elogbookagu.elogbookagu.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elogbookagu.settings')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 application = get_wsgi_application()
